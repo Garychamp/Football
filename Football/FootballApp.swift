eed3b30d5082b256f34de83328b0218f991c2c97
Garychamp <garyhoare18@gmail.com>
2025-06-01 15:59:50 +0100

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Gary Hoare on 01/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
